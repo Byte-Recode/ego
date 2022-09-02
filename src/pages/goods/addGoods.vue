9fe5f2209c999405a5b3501c660b875c48ae1674
byt <1473215569@qq.com>
2022-09-02 22:20:24 +0800

    4.1

--- src/pages/goods/addGoods.vue
+++ src/pages/goods/addGoods.vue
@@ -14,11 +14,23 @@
         <h3>商品添加</h3>
         <div class="form">
             <el-form ref="form" :model="form" label-width="80px">
-				<el-form-item label="类目选择">
-                    <el-button>类目选择</el-button>
+                <el-form-item label="类目选择">
+                    <el-button type="primary">类目选择</el-button>
                 </el-form-item>
                 <el-form-item label="商品名称">
                     <el-input v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="商品价格">
+                    <el-input v-model="form.price"></el-input>
+                </el-form-item>
+                <el-form-item label="商品数量">
+                    <el-input v-model="form.quantity"></el-input>
+                </el-form-item>
+                <el-form-item label="商品卖点">
+                    <el-input v-model="form.feature"></el-input>
+                </el-form-item>
+                <el-form-item label="商品图片">
+                    <el-button type="primary">上传图片</el-button>
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary" @click="onSubmit"
@@ -37,9 +49,9 @@ export default {
         return {
             form: {
                 name: "",
-                region: "",
-                date1: "",
-                date2: "",
+                price: "",
+                quantity: "",
+                feature: "",
                 delivery: false,
                 type: [],
                 resource: "",
@@ -64,18 +76,18 @@ export default {
         border: 1px solid #f4f4f4;
         border-radius: 5px;
         font-size: 14px;
-		margin-bottom: 10px;
+        margin-bottom: 10px;
     }
-	h3{
-		margin-left: 10px;
-	}
+    h3 {
+        margin-left: 10px;
+    }
     .form {
         background: #ffffff;
         border: 1px solid #f4f4f4;
         border-radius: 5px;
-		margin-top: 10px;
-		padding-top: 15px;
-		padding-right: 10px;
+        margin-top: 10px;
+        padding-top: 15px;
+        padding-right: 10px;
     }
 }
 </style>
